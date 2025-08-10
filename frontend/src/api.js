21289daeec3918e5e336d1c515d0378cc6498b8e
Shivxm <117083143+shivxm7@users.noreply.github.com>
2025-08-11 01:54:41 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.BACKEND_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.BACKEND_URL;
 
 // export const api = axios.create({ baseURL: BASE });
 
