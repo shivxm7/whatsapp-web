4aa287eefac12ca1a9ba722587ebe531da3fd688
Shivxm <117083143+shivxm7@users.noreply.github.com>
2025-08-11 00:55:34 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use("/conversation", conversationRoutes);
 app.use("/message", messageRoute);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
