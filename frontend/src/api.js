f4ba173059a74c3634dfbe384e7057e8aa34fbbc
Shivxm <117083143+shivxm7@users.noreply.github.com>
2025-08-11 02:06:25 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.BACKEND_URL;
+const API_BASE_URL = import.meta.env.BACKEND_URL || "https://whatsapp-web-u852.onrender.com";
 
 // export const api = axios.create({ baseURL: BASE });
 
