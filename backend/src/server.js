a81292728ddd675421fe3d3e9195c373df065f24
Shivam Sehgal <coderrshivam@gmail.com>
2025-08-11 00:06:13 +0530

    env file update

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use("/conversation", conversationRoutes);
 app.use("/message", messageRoute);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
